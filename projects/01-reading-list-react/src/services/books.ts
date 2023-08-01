4034c122d2eb2dbaf37654aa973af2a084d763fd
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-31 22:53:08 -0600

    feat(projects/01-reading-list-react): Get and set intial books from local storage

---
+++ projects/01-reading-list-react/src/services/books.ts
@@ -0,0 +1,32 @@
+import booksMocks from '../assets/books.json'
+import { Book } from '../types';
+
+export function getInitialBooks (): Book[] {
+  const lectureBooks: Book[] = getLectureBooks()
+
+  const mockedBooks = booksMocks.library.filter(({ book }) => {
+    if (lectureBooks.some(({ ISBN }) => ISBN === book.ISBN)) {
+      return false
+    }
+
+    return true
+  })
+
+  return mockedBooks.map(({ book }) => book)
+}
+
+export const getLectureBooks = (): Book[] => {
+  const localStorageBooks = localStorage.getItem('books')
+
+  return localStorageBooks 
+    ? JSON.parse(localStorageBooks)
+    : []
+}
+
+export const setLectureBooks = (books: Book[]): void => {
+  localStorage.setItem('books', JSON.stringify(books))
+}
+
+export function getGenres (): string[] {
+  return [...new Set(booksMocks.library.map(({ book }) => book.genre))]
+}
