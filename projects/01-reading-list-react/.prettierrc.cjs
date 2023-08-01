3ed0e74f3e660a7abfbc5140a91adaf16915f2b0
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-31 22:52:24 -0600

    feat(projects/01-reading-list-react): Initial project configuration

---
+++ projects/01-reading-list-react/.prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Options} */
+const config = {
+  trailingComma: "none",
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "auto",
+  jsxSingleQuote: true,
+}
+
+module.exports = config
